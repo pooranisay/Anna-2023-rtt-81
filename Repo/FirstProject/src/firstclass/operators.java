d949f78ea2b93deac3244aaf44808241eb01f8db
ANNA\Anna_ <pooranisay@yahoo.com>
2023-09-19 16:28:43 -0400

    comitted today sep 19th

---
+++ Repo/FirstProject/src/firstclass/operators.java
@@ -0,0 +1,23 @@
+package Firstclass;
+
+public class operators {
+	
+	public static void main(String args[])
+	{
+	
+	int x=3;
+	if((x>=3)&&(x<20))
+	{
+		System.out.println("the number is between 3 and 20");
+	}
+	int y=4;
+			if((y%2)==1)
+			{
+				System.out.println("this number is odd");
+			}
+			else
+			{System.out.println("this number is even");
+			}
+
+}
+}
