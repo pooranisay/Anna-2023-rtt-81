8d6d2f615665172ad49140a27419a279ce6ea68c
ANNA\Anna_ <pooranisay@yahoo.com>
2023-09-26 09:28:39 -0400

    GLAB september 26

---
+++ Repo/module303/src/GLAB/loopdemoone.java
@@ -0,0 +1,12 @@
+package GLAB;
+
+public class loopdemoone {
+	public static void main(String[] args) {
+		int n = 5;
+		// for loop
+		for (int i = 1; i <= n; ++i) {
+		System.out.println("Java is fun");
+		}
+		}
+
+}
