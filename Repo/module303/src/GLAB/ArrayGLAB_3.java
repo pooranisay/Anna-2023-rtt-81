15b55952620519b47fc3df769b1ae1a4ecc6e935
ANNA\Anna_ <pooranisay@yahoo.com>
2023-09-29 08:30:24 -0400

    Array GLAB

---
+++ Repo/module303/src/GLAB/ArrayGLAB_3.java
@@ -0,0 +1,13 @@
+package GLAB;
+
+public class ArrayGLAB_3 {
+	
+	public static void main(String[] args) {
+		String[] names = { "New York", "Dallas", "Las Vegas", "Florida" };
+		for (String name : names) {
+		System.out.println(name);
+		}
+		}
+
+
+}
